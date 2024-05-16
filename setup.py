0db180e5e17ec9c1a579774da2ef952e1c4d8be4
Yann CHOHO <chohoyann@gmail.com>
2024-05-16 14:40:08 +0200

    Innitial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Optimize a machine learning model by fusing operations.',
+    author='yann_and_paul',
+    license='MIT',
+)
